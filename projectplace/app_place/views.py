f50d2d7f3479d8b2a3cc40e3f3ac23f53cb8dea4
Vadher Divyesh Maheshbhai <102305908+divyeshvadher@users.noreply.github.com>
2023-08-26 14:37:34 +0530

    views.py

--- projectplace/app_place/views.py
+++ projectplace/app_place/views.py
@@ -5,15 +5,20 @@ from pickle import NONE
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
-from .forms import CreateUserForm
+from .forms import CreateUserForm, CreateRecForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required(login_url='stdlogin')
 def index(request):
     return render(request, 'home.html')
+@login_required(login_url='stdlogin')
+def student(request):
+    return render(request, 'student.html')
+@login_required(login_url='reclogin')
+def recruiter(request):
+    return render(request, 'recruiter.html')
 @login_required(login_url='stdlogin')
 def about(request):
    return render(request, 'about.html')
@@ -42,7 +47,7 @@ def stdLogin(request):
         
         if user is not None:
             login(request, user)
-            return redirect('home')
+            return redirect('student')
         else:
             messages.info(request, 'Username or Password is incorrect')
             return render(request, 'loginS.html', context)
@@ -53,3 +58,32 @@ def logoutUser(request):
     logout(request)
     return redirect('stdlogin')
 
+def recRegister(request):
+    form = CreateRecForm()
+    
+    if request.method == 'POST':
+        form = CreateRecForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account is created for '+user)
+            return redirect('reclogin')
+    
+    context={'form':form}
+    return render(request, 'registerR.html', context)
+def recLogin(request):
+    context={}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('recruiter')
+        else:
+            messages.info(request, 'Username or Password is incorrect')
+            return render(request, 'loginR.html', context)
+    
+    return render(request, 'loginR.html', context)
