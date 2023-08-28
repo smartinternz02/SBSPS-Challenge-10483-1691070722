464c4394c41cc626e23259dd5d57edaae4e3e852
Vadher Divyesh Maheshbhai <102305908+divyeshvadher@users.noreply.github.com>
2023-08-28 18:03:26 +0530

    urls.py

--- projectplace/app_place/urls.py
+++ projectplace/app_place/urls.py
@@ -6,7 +6,13 @@ from app_place import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('student',views.student, name='student'),
+    path('academic', views.academic, name='academic'),
+    path('skills', views.skills, name='skills'),
+    path('branch', views.branch, name='branch'),
+    path('company', views.company, name='company'),
     path('recruiter',views.recruiter, name='recruiter'),
+    path('racademic',views.racademic, name='racademic'),
+    path('rskills',views.rskills, name='rskills'),
     path("about", views.about, name='about'),
     path("contactus", views.contactus, name='contactus'),
     path("stdlogin",views.stdLogin, name='stdlogin'),
